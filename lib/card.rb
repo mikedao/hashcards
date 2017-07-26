fbe3c727180119a1ea9d33c8b5deaa845fd59936
marcoverzero <marcoverzero@gmail.com>
2017-07-26 11:22:13 -0600

    adds full card functionality of it 1

--- lib/card.rb
+++ lib/card.rb
@@ -1,1 +1,15 @@
 # Good luck!
+class Card
+
+  def initialize(card_data)
+    @card_data = card_data
+  end
+
+  def question
+  @card_data[:question]
+  end
+
+  def answer
+    @card_data[:answer]
+  end
+end
