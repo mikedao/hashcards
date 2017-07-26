b644497212e50ab1a98825ecfa0d0a9bc7d4ab48
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-26 11:57:32 -0600

    almost there on number_correct method

--- lib/round.rb
+++ lib/round.rb
@@ -6,13 +6,19 @@ class Round
   def initialize(deck)
     @deck = deck
     @guesses = []
+    @index = 0
   end
 
   def current_card
-    deck.cards.first
+    deck.cards[@index]
   end
 
   def record_guess(guess)
     guesses << Guess.new(guess, current_card)
+    @index += 1
+  end
+
+  def number_correct
+    
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -23,11 +23,21 @@ class RoundTest < Minitest::Test
     card_2 = Card.new(question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000")
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
-    round.record_guess("Juneau")
 
     assert_instance_of Card, round.current_card
     assert_equal round.current_card.answer, "Juneau"
+
+    round.record_guess("Juneau")
+
     assert_equal round.guesses.count, 1
     assert_equal round.guesses.first.feedback, "Correct!"
+    assert_equal round.current_card.answer, "93,000,000"
+
+    round.record_guess("2")
+binding.pry
+    assert_equal round.guesses.count, 2
+    assert_equal round.guesses.last.feedback, "Incorrect."
+    assert_equal round.number_correct, 1
+    assert_equal round.percent_correct, 50
   end
 end
