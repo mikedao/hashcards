39737b322006fe83995de2355024a45b7a3a52fc
Ellen Cooper <ellencooper74@gmail.com>
2017-07-26 11:43:40 -0600

    adds round and current_card, record_guess methods

---
+++ lib/round.rb
@@ -0,0 +1,16 @@
+class Round
+  attr_reader :deck, :guesses
+
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+  end
+
+  def current_card
+    @deck.cards[0]
+  end
+
+  def record_guess(guess)
+    @guesses << Guess.new(guess, current_card)
+  end
+end
---
+++ test/round_test.rb
@@ -0,0 +1,31 @@
+require_relative "test_helper"
+require "./lib/round"
+
+class RoundTest < Minitest::Test
+  def test_it_initializes_with_deck_and_empty_guesses_array
+    card_1 = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    card_2 = Card.new(question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+    assert_equal [], round.guesses
+  end
+  def test_current_card_returns_first_card_to_start
+    card_1 = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    card_2 = Card.new(question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+  end
+
+  def test_record_guess_records_guess_in_guesses
+    card_1 = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    card_2 = Card.new(question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    round.record_guess("Juneau")
+
+    assert_equal "Juneau", round.guesses[0].response
+  end
+end
