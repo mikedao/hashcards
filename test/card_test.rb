f6c3e06cfa93f76071a9f94eaec47bc4ea08cd21
Ellen Cooper <ellencooper74@gmail.com>
2017-07-26 10:54:34 -0600

    adds card class and intitialize

---
+++ test/card_test.rb
@@ -0,0 +1,10 @@
+require_relative "test_helper"
+require_relative "../lib/card"
+
+class CardTest < Minitest::Test
+  def test_it_initializes_with_question_and_answer
+    card = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    assert_equal "What is the capital of Alaska?", card.question
+    assert_equal "Juneau", card.answer
+  end
+end
