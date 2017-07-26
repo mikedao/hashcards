3da1320ac933fe1a8bc7902e63229255510d70e4
wratterman <will.ratterman@gmail.com>
2017-07-26 11:03:56 -0600

    guess tested, can take card and check the response to see if it is correct

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,3 +1,20 @@
 class Guess
+  attr_reader :response, :card
 
+  def initialize(response, card)
+    @response = response
+    @card     = card
+  end
+
+  def correct?
+    true if response == card.answer
+  end
+
+  def feedback
+    if correct?
+      "Correct"
+    else
+      "Wrong!"
+    end
+  end
 end
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,10 +1,22 @@
 require './test/test_helper'
 require './lib/guess'
+require './lib/card'
 
 class GuessTest < Minitest::Test
 
   def test_it_exists
-    guess = Guess.new
+    card = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    guess = Guess.new("Juneau", card)
     assert_instance_of Guess, guess
+  end
+
+  def test_it_takes_an_answer_and_a_card_at_initialize
+    card = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    guess = Guess.new("Juneau", card)
+
+    assert_instance_of Card, guess.card
+    assert_equal "Juneau", guess.response
+    assert guess.correct?
+    assert_equal "Correct", guess.feedback
   end
 end
