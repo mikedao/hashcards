8c70f95df7c99b7a0c7aa72f05b014b70a19c502
wratterman <will.ratterman@gmail.com>
2017-07-26 10:47:44 -0600

    tests passing for question and answer attributes, looking through coverage %

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,9 @@
 # Good luck!
 class Card
+  attr_reader :question, :answer
 
+  def initialize(question: question, answer: answer)
+    @question = question
+    @answer   = answer
+  end
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -4,9 +4,18 @@ require './lib/card'
 class CardTest < Minitest::Test
 
   def test_it_exists
-    card = Card.new
-
+    card       = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
     assert_instance_of Card, card
   end
 
+  def test_it_takes_a_question_and_answer_at_initialize
+    card       = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    expected_1 = "What is the capital of Alaska?"
+    actual_1   = card.question
+    expected_2 = "Juneau"
+    actual_2   = card.answer
+
+    assert_equal expected_1, actual_1
+    assert_equal expected_2, actual_2
+  end
 end
