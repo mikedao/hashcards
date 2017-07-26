fe55f88abc526ba67de7727642b09452deabaeec
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-26 11:11:00 -0600

    add count method to deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -13,4 +13,15 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
     assert_equal cards, deck.cards
   end
+
+  def test_it_knows_how_many_cards_in_deck
+    card_1 = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
+    card_2 = Card.new(question: "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", answer: "Mars")
+    card_3 = Card.new(question: "Describe in words the exact direction that is 697.5° clockwise from due north?", answer: "North north west")
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal cards, deck.cards
+    assert_equal 3, deck.count
+  end
 end
