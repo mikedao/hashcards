eddfac30927e64ec0b87caba6d009c7b51cc22ef
Desislava Todorova <dslavatodorova@gmail.com>
2017-07-28 22:43:09 -0600

    add def count to deck; initialize deck with default empty array; pass tests; completes IT3

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,12 @@
-require 'pry'
 class Deck
+  attr_reader :cards
 
   def initialize(cards = [])
     @cards = cards
+  end
+
+  def count
+    cards.count
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,15 +1,18 @@
-require 'test_helper'
+require_relative 'test_helper'
 require_relative '../lib/deck.rb'
+require_relative '../lib/card.rb'
 
 class DeckTest < Minitest::Test
-  def test_deck_has_cards
 
+  def test_deck_is_initialized_with_cards
     card_1 = Card.new(question: "What is the capital of Alaska?", answer: "Juneau")
     card_2 = Card.new(question: "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", answer: "Mars")
     card_3 = Card.new(question: "Describe in words the exact direction that is 697.5° clockwise from due north?", answer: "North north west")
 
     deck = Deck.new([card_1, card_2, card_3])
 
+    assert_instance_of Deck, deck
+    assert_instance_of Card, deck.cards.sample
     assert_equal [card_1, card_2, card_3], deck.cards
     assert_equal 3, deck.count
   end
